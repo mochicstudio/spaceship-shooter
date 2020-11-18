156aa5208119252455a887c23a18a15429b1a328
Andres Ruiz <andrsruiz@gmail.com>
2020-11-18 17:09:00 -0600

    Add enemy sprite

--- src/scenes/PlayGame.js
+++ src/scenes/PlayGame.js
@@ -4,10 +4,8 @@ export class PlayGame extends Phaser.Scene {
 	}
 
 	create(){
-		this.background = this.add.image(0, 0, 'background');
-		this.background.setOrigin(0, 0);
-
 		this.player_blue_one = this.add.image(this.game.config.width/2, this.game.config.height/2, 'player_blue_one');
+		this.enemy_black_one = this.add.image(this.game.config.width/4, this.game.config.height/4, 'enemy_black_one');
 
 		this.add.text(20, 20, 'Playing game', { font: '25px Arial', fill: 'yellow' });
 	}
