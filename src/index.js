863527086f66ea175c47082cc946c01cdbb0a952
Andres Ruiz <andrsruiz@gmail.com>
2020-11-18 13:04:02 -0600

    Use Scenes in main

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
+import { TitleScreen } from './scenes/TitleScreen.js';
+import { PlayGame } from './scenes/PlayGame.js';
 
-let gameScene = new Phaser.Scene('Game')
-
-gameScene.preload = function(){
-	this.load.image('background', '../src/assets/bg_castle.png')
-}
-
-gameScene.create = function(){
-	let background = this.add.sprite(0, 0, 'background')
-	// background.setOrigin(0, 0)
-	background.setPosition(50, 50)
-}
-
-let config = {
+var config = {
 	type: Phaser.AUTO,
 	width: 640,
 	height: 360,
-	scene: gameScene
-}
+	background: 0x000000,
+	scene: [TitleScreen, PlayGame]
+};
 
-let game = new Phaser.Game(config)
+var game = new Phaser.Game(config);
