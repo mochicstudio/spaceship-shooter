dd1b051e9256ec0d38e425b1461eb7f8b1d950df
Andres Ruiz <andrsruiz@gmail.com>
2020-11-19 13:11:46 -0600

    Add new background

--- src/scenes/BootGame.js
+++ src/scenes/BootGame.js
@@ -1,4 +1,5 @@
 import background from '../assets/images/backgrounds/blue.png';
+import background_title from '../assets/images/backgrounds/purple.png';
 import player_blue_one from '../assets/images/png/playerShip1_blue.png';
 import enemy_black_one from '../assets/images/png/Enemies/enemyBlack1.png';
 
@@ -9,6 +10,7 @@ export class BootGame extends Phaser.Scene {
 
 	preload(){
 		this.load.image('background', background);
+		this.load.image('background_title', background_title);
 		this.load.image('player_blue_one', player_blue_one);
 		this.load.image('enemy_black_one', enemy_black_one);
 	}
