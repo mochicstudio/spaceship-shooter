680518d241d2ad466bf4b06b60a5ff59ba8b0863
Andres Ruiz <andrsruiz@gmail.com>
2021-01-08 16:02:27 -0600

    Use tweens to move image

--- src/scenes/MochicStudio.js
+++ src/scenes/MochicStudio.js
@@ -4,6 +4,20 @@ export class MochicStudio extends Phaser.Scene {
 	}
 
 	create(){
-		this.logo = this.add.image(0, 0, 'logo').setDisplayOrigin(0, 0);
+		this.logo = this.add.image(0, 0, 'logo');
+
+		var timeline = this.tweens.createTimeline();
+
+		timeline.add({
+			targets: this.logo,
+			x: this.game.config.width / 2,
+			y: this.game.config.height / 2,
+			ease: 'Back',
+			duration: 1000,
+			repeat: 0,
+			yoyo: false
+		});
+
+		timeline.play();
 	}
 }
