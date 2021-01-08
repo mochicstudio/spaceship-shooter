52e5e49891711b50dadec407e5bb01e2a6b8e7ba
Andres Ruiz <andrsruiz@gmail.com>
2021-01-08 16:35:00 -0600

    Timeline to show logo and name

--- src/scenes/MochicStudio.js
+++ src/scenes/MochicStudio.js
@@ -4,8 +4,23 @@ export class MochicStudio extends Phaser.Scene {
 	}
 
 	create(){
-		this.logo = this.add.image(0, 0, 'logo');
+		this.createLogo();
+		this.createTitle();
+		this.createTimeLine();
+	}
 
+	createLogo(){ this.logo = this.add.image(0, 0, 'logo'); }
+
+	createTitle(){
+		this.title = this.add.text(
+			this.game.config.width / 2,
+			this.game.config.height + 100,
+			'Mochic Studio',
+			{ font: '3em Arial' }
+		).setOrigin(0.5);
+	}
+
+	createTimeLine(){
 		var timeline = this.tweens.createTimeline();
 
 		timeline.add({
@@ -13,6 +28,15 @@ export class MochicStudio extends Phaser.Scene {
 			x: this.game.config.width / 2,
 			y: this.game.config.height / 2,
 			ease: 'Back',
+			duration: 1000,
+			repeat: 0,
+			yoyo: false
+		});
+
+		timeline.add({
+			targets: this.title,
+			y: this.game.config.height - 300,
+			ease: 'Cubic',
 			duration: 1000,
 			repeat: 0,
 			yoyo: false
