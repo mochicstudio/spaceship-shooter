b91ef21a208ca4f52bd795203193e8ceff4fb742
Andres Ruiz <andrsruiz@gmail.com>
2021-01-11 13:21:22 -0600

    Set container to game title

--- src/scenes/TitleScreen.js
+++ src/scenes/TitleScreen.js
@@ -53,18 +53,35 @@ export class TitleScreen extends Phaser.Scene {
 	}
 
 	setTitle(){
-		this.add.text(
-			this.game.config.width/2,
-			this.game.config.height/4,
+		/* Store the title objects on a container */
+		this.titleContainer = this.add.container();
+		const textXAxis = 10;
+		const pixel = 1;
+		const textCenter = 0.5;
+
+		/* We use the constants and variables to set
+		 * relative position on all the childs of the
+		 * container and the container itself.
+		 * This way, they'll never be on top
+		 * of each other. */
+		let gameNamePos = { x: textXAxis, y: 10 };
+		let gameName = this.add.text(
+			gameNamePos.x,
+			gameNamePos.y,
 			'SPACESHIP SHOOTER',
 			{ font: '3em Arial bold', color: 'orange' }
-		).setOrigin(0.5);
-		this.add.text(
-			this.game.config.width/2,
-			this.game.config.height/4 + 30,
+		).setOrigin(textCenter);
+
+		let gameInfoPos = { x: textXAxis, y: gameName.height + pixel };
+		let gameInfo = this.add.text(
+			gameInfoPos.x,
+			gameInfoPos.y,
 			'GameOff 2020',
 			{ font: '2em Arial' }
-		).setOrigin(0.5);
+		).setOrigin(textCenter);
+
+		this.titleContainer.add([gameName, gameInfo]);
+		this.titleContainer.setPosition(this.game.config.width / 2, pixel * 5);
 	}
 
 	playerSelection(){
