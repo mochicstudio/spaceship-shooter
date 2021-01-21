7ca0b35669df37b1add0e937eb7f453b67ce29ec
Andres Ruiz <andrsruiz@gmail.com>
2021-01-21 13:42:31 -0600

    Start new game

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -4,18 +4,40 @@ export class GameOver extends Phaser.Scene {
 	}
 
 	create(data){
+		this.setText(data.score);
+		this.setKeyboardEvents();
+	}
+
+	setText(score){
+		let accumulateY = 0;
+
 		this.gameOver = this.add.text(
 			this.game.config.width / 2,
 			this.game.config.height / 2,
 			'Game Over',
 			{ font: '3em Arial' }
 		).setOrigin(0.5);
+		accumulateY += this.gameOver.height;
 
 		this.scoreText = this.add.text(
 			this.game.config.width / 2,
-			this.game.config.height / 2 + this.gameOver.height,
-			'Your score: ' + data.score,
+			this.game.config.height / 2 + accumulateY,
+			'Your score: ' + score,
 			{ font: '2em Arial' }
 		).setOrigin(0.5);
+		accumulateY += this.scoreText.height;
+
+		this.playAgain = this.add.text(
+			this.game.config.width / 2,
+			this.game.config.height / 2 + accumulateY,
+			'Press Spacebar to Go to Title Screen',
+			{ font: '2em Arial' }
+		).setOrigin(0.5);
+	}
+
+	setKeyboardEvents(){
+		this.input.keyboard.on('keydown-SPACE', () => {
+			this.scene.start('titleScreen');
+		});
 	}
 }
