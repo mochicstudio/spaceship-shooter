76b164fadd9af7cc6c398d45727e8140eeeaf46b
Andres Ruiz <andrsruiz@gmail.com>
2021-06-03 22:55:50 -0500

    Use gamestate on main file

--- main.lua
+++ main.lua
@@ -1,57 +1,7 @@
-local currentColor = require('current_color')
-local keyMap = require('keymap')
-local seconds = 0
-local paused = false
-local square = require('entities/square')
-local ground = require('entities/ground')
-local myWorld = require('world')
+local Gamestate = require('libs/hump/gamestate')
+local game = require('game')
 
-love.update = function(dt)
-	if not paused then
-		seconds = seconds + dt
-		myWorld:update(dt)
-	end
-end
-
-love.draw = function()
-	love.graphics.setColor(currentColor)
-	love.graphics.print('Love2D version is ' .. love.getVersion(), 1, 1)
-
-	-- Clock
-	local clock = 'Seconds ' .. math.floor(seconds)
-	love.graphics.print(clock, 1, 15)
-
-	if paused then
-		love.graphics.print('Paused', love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
-	end
-
-	-- Draw the square
-	love.graphics.polygon('fill', square.body:getWorldPoints(square.shape:getPoints()))
-	love.graphics.polygon('fill', ground.body:getWorldPoints(ground.shape:getPoints()))
-end
-
-love.focus = function(focus)
-	-- Pause the game automatically
-	paused = not focus
-
-	if focus then
-		print("Window is focused")
-	else
-		print("Window is not focused")
-	end
-end
-
--- rgb colors and exit the game
-love.keypressed = function(pressedKey)
-	local response
-	if keyMap[pressedKey] then
-		response = keyMap[pressedKey]()
-
-		-- Pause and unpause the game
-		if response ~= 'pause' then
-			currentColor = response
-		else
-			paused = not paused
-		end
-	end
+love.load = function()
+	Gamestate.registerEvents()
+	Gamestate.switch(game)
 end
