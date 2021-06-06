a0f5f8371ecbf474c6ea53b73634bea84ef25793
Andres Ruiz <andrsruiz@gmail.com>
2021-06-06 14:48:00 -0500

    Use Splash Screen in Main

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 local Gamestate = require('libs/hump/gamestate')
+local splashScreen = require('states/splash-screen')
 local game = require('states/game')
 
 love.load = function()
 	Gamestate.registerEvents()
-	Gamestate.switch(game)
+	Gamestate.switch(splashScreen)
 end
