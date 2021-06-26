fb76bdc8f7b28d921bfad7db7365583c61ec513a
Andres Ruiz <andrsruiz@gmail.com>
2021-06-26 13:01:47 -0500

    Load Mochic Studio Logo

--- states/splash-screen.lua
+++ states/splash-screen.lua
@@ -1,8 +1,20 @@
 local splashScreen = {}
 local HALF = 2
 local LINE = 15
+local MOCHIC_STUDIO_DATA
+local MOCHIC_STUDIO_IMAGE
+
+function splashScreen:init()
+	MOCHIC_STUDIO_DATA = love.image.newImageData('sprites/mochicstudio.png')
+	MOCHIC_STUDIO_IMAGE = love.graphics.newImage(MOCHIC_STUDIO_DATA)
+
+	print(MOCHIC_STUDIO_IMAGE:getWidth())
+	print(MOCHIC_STUDIO_IMAGE:getHeight())
+end
 
 function splashScreen:draw()
+	love.graphics.draw(MOCHIC_STUDIO_IMAGE,10,10)
+
 	local yAxis = love.graphics.getHeight() / 2
 	-- printf creates a box around the text so we can center it
 	love.graphics.printf('Booting ...', 0, yAxis, love.graphics.getWidth(), 'center')
