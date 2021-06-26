03243ee7112e1b0ac0ec6e192b33e96c9c8da51b
Andres Ruiz <andrsruiz@gmail.com>
2021-06-26 14:15:20 -0500

    Tweeing Image Pos

--- states/splash-screen.lua
+++ states/splash-screen.lua
@@ -5,16 +5,19 @@ local LINE = 15
 local MOCHIC_STUDIO_DATA
 local MOCHIC_STUDIO_IMAGE
 local COLOR
+local POS = {x = -320}
 
 function splashScreen:init()
-	COLOR = {0*255, 0*255, 0*255, 1}
-	Timer.tween(10, COLOR, {1*255, 1*255, 1*255, 1}, 'in-out-quad')
+	COLOR = {0.0*255, 0.0*255, 0.0*255, 1}
+	Timer.tween(10, COLOR, {0.5*255, 0.0*255, 1.0*255, 1}, 'bounce')
 
 	MOCHIC_STUDIO_DATA = love.image.newImageData('sprites/mochicstudio.png')
 	MOCHIC_STUDIO_IMAGE = love.graphics.newImage(MOCHIC_STUDIO_DATA)
 
 	print(MOCHIC_STUDIO_IMAGE:getWidth())
 	print(MOCHIC_STUDIO_IMAGE:getHeight())
+
+	Timer.tween(3, POS, {x = 10}, 'bounce')
 end
 
 function splashScreen:update(dt)
@@ -23,7 +26,7 @@ end
 
 function splashScreen:draw()
 	love.graphics.setBackgroundColor(COLOR)
-	love.graphics.draw(MOCHIC_STUDIO_IMAGE,10,10)
+	love.graphics.draw(MOCHIC_STUDIO_IMAGE,POS.x,10)
 
 	local yAxis = love.graphics.getHeight() / 2
 	-- printf creates a box around the text so we can center it
