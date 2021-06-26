fa0debd3ea8dad24a7bfdbf484f8a1063bb176e4
Andres Ruiz <andrsruiz@gmail.com>
2021-06-26 17:06:22 -0500

    Tween Studio Text

--- states/splash-screen.lua
+++ states/splash-screen.lua
@@ -10,6 +10,13 @@ local MOCHIC_STUDIO_IMAGE = {
 		y = 0
 	}
 }
+local MOCHIC_STUDIO_TEXT = {
+	text = 'Mochic Studio',
+	pos = {
+		x = 0,
+		y = 0
+	}
+}
 
 function splashScreen:init()
 	MOCHIC_STUDIO_DATA = love.image.newImageData('sprites/mochicstudio.png')
@@ -18,10 +25,19 @@ function splashScreen:init()
 	MOCHIC_STUDIO_IMAGE.pos.x = love.graphics.getWidth() / HALF - MOCHIC_STUDIO_IMAGE.image:getWidth() / HALF
 	MOCHIC_STUDIO_IMAGE.pos.y = -1 * MOCHIC_STUDIO_IMAGE.image:getHeight()
 
+	MOCHIC_STUDIO_TEXT.pos.y = love.graphics.getHeight()
+
 	Timer.tween(
 		2,
 		MOCHIC_STUDIO_IMAGE,
 		{ pos = {y = love.graphics.getHeight() / HALF - MOCHIC_STUDIO_IMAGE.image:getHeight() / HALF} },
+		'bounce'
+	)
+
+	Timer.tween(
+		2,
+		MOCHIC_STUDIO_TEXT,
+		{ pos = {y = love.graphics.getHeight() / HALF + MOCHIC_STUDIO_IMAGE.image:getHeight() / HALF} },
 		'bounce'
 	)
 end
@@ -31,19 +47,27 @@ function splashScreen:update(dt)
 end
 
 function splashScreen:draw()
-	love.graphics.draw(
-		MOCHIC_STUDIO_IMAGE.image,
-		MOCHIC_STUDIO_IMAGE.pos.x,
-		MOCHIC_STUDIO_IMAGE.pos.y
-	)
-
-	local yAxis = love.graphics.getHeight() / HALF
+	local yAxis = 10 -- love.graphics.getHeight() / HALF
 	-- printf creates a box around the text so we can center it
 	love.graphics.printf('Booting ...', 0, yAxis, love.graphics.getWidth(), 'center')
 	yAxis = yAxis + LINE
 	love.graphics.printf('Spaceship Shooter v0.1', 0, yAxis, love.graphics.getWidth(), 'center')
 	yAxis = yAxis + LINE
 	love.graphics.printf('LÖVE2D v' .. love.getVersion(), 0, yAxis, love.graphics.getWidth(), 'center')
+
+	love.graphics.draw(
+		MOCHIC_STUDIO_IMAGE.image,
+		MOCHIC_STUDIO_IMAGE.pos.x,
+		MOCHIC_STUDIO_IMAGE.pos.y
+	)
+
+	love.graphics.printf(
+		MOCHIC_STUDIO_TEXT.text,
+		MOCHIC_STUDIO_TEXT.pos.x,
+		MOCHIC_STUDIO_TEXT.pos.y,
+		love.graphics.getWidth(),
+		'center'
+	)
 end
 
 return splashScreen
