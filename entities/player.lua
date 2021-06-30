9f1138336e171ee4847a01d0789246d839486bb3
Andres Ruiz <andrsruiz@gmail.com>
2021-06-29 21:46:00 -0500

    Player Entity

---
+++ entities/player.lua
@@ -0,0 +1,15 @@
+local world = require('world')
+
+-- Player
+local player = {}
+
+player.body = love.physics.newBody(world, 200, 200, 'dynamic')
+player.body.setMass(player.body, 32)
+player.shape = love.physics.newPolygonShape(100, 100, 100, 200, 200, 200, 200, 100)
+player.fixture = love.physics.newFixture(player.body, player.shape)
+player.fixture:setRestitution(0.75) -- Less than 1 the body will stop bouncing
+
+-- Set fixture ID
+player.fixture:setUserData(player)
+
+return player
