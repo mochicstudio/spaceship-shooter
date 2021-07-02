c8737a377317582479a3c2c399d132b296f0b458
Andres Ruiz <andrsruiz@gmail.com>
2021-07-01 22:46:37 -0500

    Create Input Table

---
+++ input.lua
@@ -0,0 +1,55 @@
+-- Input Service
+local input = {}
+-- Specific user inputs to game actions
+local pressFunctions = {}
+local releaseFunctions = {}
+
+-- Look and execute corresponding action to specific key presses
+input.press = function(pressedKey)
+	if pressFunctions[pressedKey] then
+		return pressFunctions[pressedKey]()
+	end
+end
+
+-- Look and execute corresponding action to specific key releases
+input.release = function(releasedKey)
+	if releaseFunctions[releasedKey] then
+		return releaseFunctions[releasedKey]()
+	end
+end
+
+-- Window focusing/unfocusing
+input.toggleFocus = function(focused)
+	if not focused then
+		return true
+	end
+end
+
+-- Functions itselfs
+-- Press Functions
+pressFunctions.left = function()
+	input.buttonLeft = true
+end
+
+pressFunctions.right = function()
+	input.buttonRight = true
+end
+
+pressFunctions.escape = function()
+	love.event.quit()
+end
+
+pressFunctions.space = function()
+	return 'pause'
+end
+
+-- Release Functions
+releaseFunctions.left = function()
+	input.buttonLeft = false
+end
+
+releaseFunctions.right = function()
+	input.buttonRight = false
+end
+
+return input
