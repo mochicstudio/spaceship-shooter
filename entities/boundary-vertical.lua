4a2bd011c705915115ff0ae8f63b798d89f9b11d
Andres Ruiz <andrsruiz@gmail.com>
2021-07-03 10:04:36 -0500

    Vertical Boundary

---
+++ entities/boundary-vertical.lua
@@ -0,0 +1,18 @@
+local world = require('world')
+
+return function(posX)
+	local boundary = {}
+
+	boundary.body = love.physics.newBody(world, posX, 0, 'static')
+	boundary.shape = love.physics.newRectangleShape(1, love.graphics.getHeight())
+	boundary.fixture = love.physics.newFixture(boundary.body, boundary.shape)
+	boundary.fixture:setUserData(boundary)
+
+	-- As for boundaries we won't need to write explicitlly to draw
+	-- the entity because is out of the screen game but for code convention
+	-- we still write the draw function.
+	boundary.draw = function(self)
+	end
+
+	return boundary
+end
