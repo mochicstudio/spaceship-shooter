e882de3063bdcaefc04b45e20ec99bedd4c6666d
Andres Ruiz <andrsruiz@gmail.com>
2021-07-03 10:04:46 -0500

    Draw Vertical Boundaries

--- entities.lua
+++ entities.lua
@@ -1,8 +1,12 @@
 local spritesheet = require('entities/spritesheet')
 local player = require('entities/player')
+local boundaryLeft = require('entities/boundary-vertical')
+local boundaryRight = require('entities/boundary-vertical')
 
 return function()
 	local entities = {
+		boundaryLeft(-1),
+		boundaryRight(love.graphics.getWidth() + 1),
 		player(spritesheet.img)
 	}
 
